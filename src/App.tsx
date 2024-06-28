8e047b57a1d257c4bff70013265d485b2ea97aa1
bestdevelope <leabouillot7@gmail.com>
2024-06-28 14:24:13 +0200

    serch movie

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,44 @@
 import { useState } from 'react'
+import { motion } from 'framer-motion'
 
 export default function App() {
+
   const searchMovie = (e) => {
-     e.preventDefault()
-     console.log("La fontion searchMovie")
+    e.preventDefault()
+    console.log("La fonction searchMovie a été appelée")
   }
 
   return (
-    <main className='bg-gradient-to-bl from-neutral-800 to-red-500'>
-      <h1>Cinemax</h1>
-      <h3>Trouvez les infos de films et séries en 2 clicks !
-      </h3>
-      <form onSubmit={searchMovie}>
-        <input type="text" className='border' />
-        <button className=''>Rechercher</button>
-      </form>
-    </main>
+      <main>
+        <motion.div
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <h1>Cinemax</h1>
+        </motion.div>
+        <motion.div
+        
+        >
+        <h3>Trouvez les infos de films et de séries en 2 clics !</h3>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <form onSubmit={searchMovie}>
+            <input 
+            type="text"
+            />
+            <button>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
+              </svg>
+            </button>
+          </form>
+        </motion.div>
+      </main>
 
   )
 }
-
